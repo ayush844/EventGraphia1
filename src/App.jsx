368a5153ecf4b7586e30c02aea123e275f13ed72
ayush844 <ayush.xyz1625@gmail.com>
2025-01-07 23:00:56 +0530

    Add image card, spinner, and initial logic for infinite scrolling
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ImageCard from "./Components/ImageCard";
+import { HashLoader } from "react-spinners";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
+
+  const fetchImages = (count) => {
+    const start = images.length + 1;
+    const end = start + count - 1;
+    const newImages = [];
+    for (let i = start; i <= end; i++) {
+      newImages.push({
+        thumbnail: `https://placehold.co/200x200/jpg?text=${i}`,
+        fullView: `https://placehold.co/2000x2000/jpg?text=${i}`,
+        download: `https://placehold.co/3900x3900/jpg?text=${i}`,
+        id: i,
+      });
+    }
+    setImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  useEffect(() => {
+    fetchImages(30);
+  }, []);
+
+  // fetchImages(30);
+
+  const openFullView = (image) => {
+    console.log(image.id);
+  };
+
+  console.log(images);
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+    <div className="max-w-[100vw] overflow-x-hidden min-h-[100vh] px-2 ">
+      <div className="flex flex-col items-center h-full  py-2 gap-10 md:gap-16  overflow-x-hidden">
+        <h1 className=" text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold  text-black font-mono underline italic">
+          Image Gallery
+        </h1>
+
+        <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 gap-2 md:gap-4 lg:gap-10">
+          {images.map((image) => (
+            <ImageCard
+              key={image.id}
+              image={image}
+              onClick={() => openFullView(image)}
+            />
+          ))}
+        </div>
+        <HashLoader color="#440080" />
+      </div>
+    </div>
   );
 }
 
---
+++ src/Components/ImageCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ImageCard = ({ image, onClick }) => {
+  return (
+    <div
+      className="border rounded-lg overflow-hidden  hover:shadow-lg hover:shadow-black cursor-pointer hover:scale-105 transform transition duration-300 ease-in-out"
+      onClick={onClick}
+    >
+      <img
+        src={image.thumbnail}
+        alt={`Thumbnail ${image.id}`}
+        className="w-full h-36 md:h-40 lg:h-48 object-cover"
+      />
+    </div>
+  );
+};
+
+export default ImageCard;
