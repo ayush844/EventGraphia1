41a8598704f7146472dff62c3760bbc0a859cbce
ayush844 <ayush.xyz1625@gmail.com>
2025-01-10 11:16:51 +0530

    change name of download image
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Components/FullViewModal.jsx
+++ src/Components/FullViewModal.jsx
@@ -22,7 +22,7 @@ const FullViewModal = ({
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.download = "downloaded-image.jpg";
+      link.download = `downloaded-image${currentIndex + 1}.jpg`;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
